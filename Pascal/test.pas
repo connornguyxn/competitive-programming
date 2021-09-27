d533e53bd2f06185c161e73c7e435d4dc18d2402
inf1nityonthemic <connornguyen.dev@gmail.com>
2021-09-27 15:42:03 +0700

    Adding some Pascal algorithms

--- Pascal/test.pas
+++ Pascal/test.pas
@@ -1,4 +1,22 @@
+function mu(a:real; n:longint):real;
+var i:longint;
 begin
-assign(input,'test.inp');reset(input);
-assign(output,'test.out');rewrite(output);
+    mu:=1;
+    for i:=1 to n do
+        mu:=mu*a;
+end;
+/////////////////////////////
+function binet(n:longint):longint;
+var phi,psi:real;
+begin
+    phi:= (1 + sqrt(5)) / 2;
+    psi:= (1 - sqrt(5)) / 2;
+    exit(round((mu(phi,n) - mu(psi,n)) / sqrt(5)));
+end;
+
+///////////////////////////
+var n:longint;
+begin
+readln(n);
+writeln(binet(n));
 end.
