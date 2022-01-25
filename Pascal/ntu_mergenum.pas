a2e59eac193d55f3e0fabbe436825b7ea40611f3
Connor Nguyen <76279876+connornguyxn@users.noreply.github.com>
2022-01-25 09:17:26 +0700

    Update ntu_mergenum.pas

--- Pascal/ntu_mergenum.pas
+++ Pascal/ntu_mergenum.pas
@@ -1,28 +1,23 @@
+//coded w/ <3 by connornguyxn
 var a,b,c,mx,mn:ansistring;
 /////////////////////////////////////////////
 function max(a,b:ansistring):ansistring;
-begin
-if a>b then
-    exit(a);
-exit(b);
-end;
-//////////
-function min(a,b:ansistring):ansistring;
-begin
-if a<b then
-    exit(a);
-exit(b);
-end;
+    /////////////////
+    begin
+    if (a > b) then
+        exit(a);
+    exit(b);
+    end;
 //////////////
 procedure try(a,b,c:ansistring);
     begin
-    if (length(a)=0) or (length(b)=0) then
+    if (length(a) = 0) or (length(b) = 0) then
         begin
-        c:=c+max(a,b);
-        if c>mx then
-            mx:=c;
-        if (c<mn) or (mn='') then
-            mn:=c;
+        c := c + max(a,b);
+        if (c > mx) then
+            mx := c;
+        if (c < mn) or (mn = '') then
+            mn := c;
         end
     else
         begin
@@ -32,13 +27,11 @@ procedure try(a,b,c:ansistring);
     end;
 /////////////////////////////////////////////
 begin
-//readln(a);
 readln(b);
-//13 26
-a:=copy(b,1,pos(' ',b)-1);
+a := copy(b,1,pos(' ',b)-1);
 delete(b,1,pos(' ',b));
-mx:='';
-mn:='';
+mx := '';
+mn := '';
 try(a,b,'');
 writeln(mn);
 writeln(mx);
