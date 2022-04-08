b5a3e0a55b155db1064399225b858e4c2586c41b
connornguyxn <connornguyen.dev@gmail.com>
2022-04-08 20:09:30 +0700

    The start  of a new journey with C++

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,6 +1,12 @@
+// CPP program for the above approach
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
+// Driver Code
 int main() {
-    cout << "Hello world!" << endl;
+    vector<int> A = { 20, 10, 30, 100, 110 };
+    int k = 85;
+ 
+    // Function Call
+    cout << maxSum(A, k) << endl;
 }
